36a6547375e945e64a628075cdc177c77cc578e0
Vraj11590 <vj11590@gmail.com>
2013-04-13 17:29:38 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+af
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+
+    <head>
+
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+
+        <title></title>
+        <link rel="stylesheet" href="style.css" type="text/css" />
+
+        <script src="script.js" type="text/javascript"></script>
+
+    </head>
+
+    <body>
+
+
+	<form action="front/clientproxy.php" method="post">
+
+ 		UCID: <input type="text" name="ucid" />
+		<br/>
+ 		Password: <input type="text" name="password" />
+
+ 		<input type="submit" />
+
+ 	</form>
+ 
+    </body>
+
+</html>
