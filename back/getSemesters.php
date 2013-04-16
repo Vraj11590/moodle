f59dba6431b8678cbcd95316e82c0640859db7f6
Giaspur <gt35@njit.edu>
2013-04-16 18:22:38 -0400

    fixeds someting in model php placeholders

--- back/getSemesters.php
+++ back/getSemesters.php
@@ -1,5 +1,5 @@
 <?php
 //gets information from database, places into array with correct tags and echoes it encoded in json.
-
+$array('semseters' => array('20121','20122','20123','20131','20132','20133'));
 echo json_encode($array);
 ?>
--- front/clientproxy.php
+++ front/clientproxy.php
@@ -1,46 +1,46 @@
 <?php
-// $url = 'http://web.njit.edu/~gt35/moodle/middle/login_control.php';
-// $url = 'http://web.njit.edu/~vp78/moodle/middle/login_control.php';
-if(isset($_SESSION['ucid'])){
-	header("Location: loggedin.php");
-	//$postdata = array('ucid' => $_SESSION["ucid"], 'password' => $_SESSION["password"]);
+	// $url = 'http://web.njit.edu/~gt35/moodle/middle/login_control.php';
+	// $url = 'http://web.njit.edu/~vp78/moodle/middle/login_control.php';
+	if(isset($_SESSION['ucid'])){
+		header("Location: loggedin.php");
+		//$postdata = array('ucid' => $_SESSION["ucid"], 'password' => $_SESSION["password"]);
 	}
 	else{
-			header("Location: http://localhost/moodle");
-		}
+		header("Location: http://localhost/moodle");
+	}
+	
+	$url = 'http://localhost/moodle/middle/login_control.php';
+	$postdata = array('ucid' => $_POST["ucid"], 'password' => $_POST["password"]);
+	
+	$c = curl_init();
+	curl_setopt($c, CURLOPT_FOLLOWLOCATION,true);
+	curl_setopt($c, CURLOPT_HTTPHEADER, array('Content-Type' => 'application/xml'));
+	curl_setopt($c, CURLOPT_URL, $url);
+	curl_setopt($c, CURLOPT_POST, true);
+	curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($c, CURLOPT_POSTFIELDS,$postdata);
+	$result = curl_exec ($c); 
+	curl_close ($c); 
+	
+	
+	if($result){
 		
-$url = 'http://localhost/moodle/middle/login_control.php';
-$postdata = array('ucid' => $_POST["ucid"], 'password' => $_POST["password"]);
-
-$c = curl_init();
-curl_setopt($c, CURLOPT_FOLLOWLOCATION,true);
-curl_setopt($c, CURLOPT_HTTPHEADER, array('Content-Type' => 'application/xml'));
-curl_setopt($c, CURLOPT_URL, $url);
-curl_setopt($c, CURLOPT_POST, true);
-curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($c, CURLOPT_POSTFIELDS,$postdata);
-$result = curl_exec ($c); 
-curl_close ($c); 
-
-
-if($result){
-	
-				$result = json_decode($result);
-				session_start();
-				$_SESSION['ucid'] = $result['ucid'];
-                $_SESSION['type'] = $result['type'];
-                $_SESSION['name'] = $result['name'];
-                $_SESSION['user_logged_in'] = 1;
-                        
-                 setcookie("ucid", $result_row->ucid, time() + (3600*24*100));
-                 setcookie("type", $result_row->type, time() + (3600*24*100));
-                 header("Location: loggedin.php");
-	// $rr = json_decode($result);
-	// $flag = 1;
-	// $name = $rr->name;
-	
-	// if($flag == 1)
-	// {
+		$result = json_decode($result);
+		session_start();
+		$_SESSION['ucid'] = $result['ucid'];
+		$_SESSION['type'] = $result['type'];
+		$_SESSION['name'] = $result['name'];
+		$_SESSION['user_logged_in'] = 1;
+		
+		setcookie("ucid", $result_row->ucid, time() + (3600*24*100));
+		setcookie("type", $result_row->type, time() + (3600*24*100));
+		header("Location: loggedin.php");
+		// $rr = json_decode($result);
+		// $flag = 1;
+		// $name = $rr->name;
+		
+		// if($flag == 1)
+		// {
 		// $host  = $_SERVER['HTTP_HOST'];
 		// $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 		// $extra = "loggedin.php?ucid=".$_POST["ucid"]."&"."name=".$name;
@@ -50,32 +50,32 @@ if($result){
 		
 		// //header("Location: http://$host$uri/$extra");
 		// header("Location: loggedin.php");
-	// }
-	// elseif($flag == 0)
-	// {
+		// }
+		// elseif($flag == 0)
+		// {
 		// $_SESSION['user_logged_in'] = 0;
 		// $url = 'web.njit.edu/~vp78/moodle/index.php?password=0';
 		// header("Location: http://$url");
-	// }
-	// else
-	// {
+		// }
+		// else
+		// {
 		// echo "something is wrong";
-	// }
-
-}else{
-	 header("Location: http://localhost/moodle");
-	 }
-	 
-if(isset($_GET['logout'])){
+		// }
+		
+		}else{
+		header("Location: http://localhost/moodle");
+	}
 	
+	if(isset($_GET['logout'])){
+		
 		$_SESSION = array();
         session_destroy();
         $user_is_logged_in = false;
         //$messages[] = "You have been logged out.";
         //echo"Logout finished.";
-}
+	}
 	//header('HTTP/1.1 500 Internal Server Error');
 	
-
-
+	
+	
 ?>
