afe43ed3a665a488256ae289e60889d30446daaf
Giaspur <gt35@njit.edu>
2013-04-18 01:21:16 -0400

    simpler index, compatible with header

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <?php
-//header("Location: http://localhost/moodle/front/");
+include('resources/header.php');
 
-$loadpath = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]/front/";
-
-//echo $actual_link;
-
-//header("Location: http://web.njit.edu/~vp78/localhost/moodle/front/");
-header("Location: $loadpath");
-
+header("Location: $urlPath/front/");
 ?>
 
