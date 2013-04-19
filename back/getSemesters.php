ee0543a90cb09191acaac9168658f1ea82607d13
thh4 <thh4@njit.edu>
2013-04-18 23:35:21 -0300

    Update getSemesters.php

--- back/getSemesters.php
+++ back/getSemesters.php
@@ -1,20 +1,17 @@
 <?php
 //gets information from database, places into array with correct tags and echoes it encoded in json.
 //passed through curl from control.
-<?php
-
 
 $link = mysql_connect('sql.njit.edu', 'thh4', 'yapping45');
 if (!$link) {
-    die(' Could not connect: ' . mysql_error());//Connected successfully
-
+    		die(' Could not connect: ' . mysql_error());//Connected successfully
+	    }
 mysql_select_db("thh4") or die(mysql_error());
 
-$ucid = $_POST['ucid'];
-  $result = mysql_query(" 	SELECT DISTINCT s.semesterid AS semesterid
+	$ucid = $_POST['ucid'];
+  	$result = mysql_query(" SELECT DISTINCT s.semesterid AS semesterid
   				FROM sections AS s, enrolled AS e
-				WHERE e.crn = s.crn
-															AND e.ucid =  '".ucid."'");
+				AND e.ucid =  '".ucid."'");
 	   while($row = mysql_fetch_array($result)){															
        echo json_encode(array($row['semesterid']));
        
