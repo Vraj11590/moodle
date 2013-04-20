e59180f6921b7c6c55a4229bc8203dbd6bbf37a9
thh4 <thh4@njit.edu>
2013-04-20 16:08:07 -0300

    Update get.php

--- back/get.php
+++ back/get.php
@@ -15,14 +15,45 @@
 		
 	function getSemesters($ucid){
 		
-		$q =	"SELECT DISTINCT s.semesterid  
-		FROM sections AS s, enrolled AS e
-		WHERE e.crn = s.crn
-		AND e.ucid = '".$ucid."'";
+		$q =   "SELECT DISTINCT s.semesterid  
+			
+			FROM 	sections AS s, enrolled AS e
+			WHERE	e.crn = s.crn
+			AND 	e.ucid = '".$ucid."'";
+	
 		return array('semesters' => getElements($q));
 		
 	}
 	
+/// function to get classes in semester
+	function getClasses($ucid,$semesterid){
+		$q=	"SELECT c.courseid AS courseid, c.coursename AS coursename,
+				s.crn AS crn, s.sectionid AS sectionid,
+				u.name AS teachername
+			
+			FROM 	sections AS s, enrolled AS e, courses AS c, users AS u
+			WHERE 	e.ucid = '".$ucid."'
+			AND 	s.semesterid = '".$semesterid."'
+			AND 	e.crn = s.crn
+			AND 	s.courseid = c.courseid
+			AND 	s.teacherid = u.ucid";
+		
+		return array('classes' => getElements($q);
+	}
+
+///function to get posts in each class
+	
+	function getPosts($crn){
+		$q=	"SELECT ucid,post_title,post_text
+			
+			FROM posts
+			WHERE crn='".$crn."'";
+
+	return array('posts' => getElements($q);
+	}
+	
+
+
 	if(isset($_GET['f'])){// check if function field is set
 		$f = $_GET['f'];
 		$u = $_POST['ucid'];
