7e56284a8533109e6162d07bb196f34c464b95d0
Giaspur <gt35@njit.edu>
2013-04-24 19:09:05 -0400

    more minor fix

--- back/get.php
+++ back/get.php
@@ -5,70 +5,70 @@
 	function getElements($q){
 		$a = array();//temp array to store shit
 		$con = new mysqli(db_host, db_user, db_pass, db_name); 
-			$result = $con->query($q);
-			while($row = $result->fetch_row()){
-				
-				array_push($a,$row[0]);
-			}
-			return $a;
+		$result = $con->query($q);
+		while($row = $result->fetch_row()){
+			
+			array_push($a,$row[0]);
 		}
-		
+		return $a;
+	}
+	
 	function getSemesters($ucid){
 		
 		$q =   "SELECT DISTINCT s.semesterid  
-			
-			FROM 	sections AS s, enrolled AS e
-			WHERE	e.crn = s.crn
-			AND 	e.ucid = '".$ucid."'";
-	
+		
+		FROM 	sections AS s, enrolled AS e
+		WHERE	e.crn = s.crn
+		AND 	e.ucid = '".$ucid."'";
+		
 		return array('semesters' => getElements($q));
 		
 	}
 	
-/// function to get classes in semester
+	/// function to get classes in semester
 	function getClasses($ucid,$semesterid){
 		$q=	"SELECT c.courseid AS courseid, c.coursename AS coursename,
-				s.crn AS crn, s.sectionid AS sectionid,
-				u.name AS teachername
-			
-			FROM 	sections AS s, enrolled AS e, courses AS c, users AS u
-			WHERE 	e.ucid = '".$ucid."'
-			AND 	s.semesterid = '".$semesterid."'
-			AND 	e.crn = s.crn
-			AND 	s.courseid = c.courseid
-			AND 	s.teacherid = u.ucid";
+		s.crn AS crn, s.sectionid AS sectionid,
+		u.name AS teachername
+		
+		FROM 	sections AS s, enrolled AS e, courses AS c, users AS u
+		WHERE 	e.ucid = '".$ucid."'
+		AND 	s.semesterid = '".$semesterid."'
+		AND 	e.crn = s.crn
+		AND 	s.courseid = c.courseid
+		AND 	s.teacherid = u.ucid";
 		
 		return array('classes' => getElements($q));
 	}
-
-///function to get posts in each class
+	
+	///function to get posts in each class
 	
 	function getPosts($crn){
 		$q=	"SELECT ucid,post_title,post_text
-			
-			FROM posts
-			WHERE crn='".$crn."'";
-
-	return array('posts' => getElements($q));
+		
+		FROM posts
+		WHERE crn='".$crn."'";
+		
+		return array('posts' => getElements($q));
 	}
 	
-
-
+	
+	
 	if(isset($_GET['f'])){// check if function field is set
 		$f = $_GET['f'];
 		//$u = 'gt35';
 		$u = $_POST['ucid'];
-		$s = $_POST['semesterid'];
-		$c = $_POST['crn'];
 		
 		if($f == 'getSemesters'){
 			echo json_encode(getSemesters($u));
 		}
 		if($f == 'getClasses'){
+			$s = $_POST['semesterid'];
 			echo json_encode(getClasses($u,$s));
 			
 		}
 		if($f == 'getPosts'){
+			$c = $_POST['crn'];
 			
 			echo json_encode(getPosts($c));
 			
