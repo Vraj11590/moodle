2de1c7acc249c61b8ad58abdf5ecca7393b30a7c
Giaspur <gt35@njit.edu>
2013-04-26 23:08:45 -0400

    middle edit

--- middle/control.php
+++ middle/control.php
@@ -3,7 +3,8 @@
 	include('../resources/header.php');
 	$functionCall = false;
 	$auth = false;
-	$data = $_POST;
+	$data = $_POST
+	$con = new mysqli(db_host, db_user, db_pass, db_name); 	;
 	include('return.php');
 	include('login_control.php');
 ?>		
--- middle/return.php
+++ middle/return.php
@@ -1,7 +1,7 @@
 <?php
 	/*function list to return certain data encoded in json for front
 	*/
-
+	
 	function getJSON($value,$postval,$urlPath){
 		$url = $urlPath.'/back/get.php?f='.$value;
 		$postdata = $postval;
@@ -12,16 +12,38 @@
 		curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($c, CURLOPT_POST, true);
 		curl_setopt($c, CURLOPT_POSTFIELDS,$postdata);
-
+		if(curl_exec($c) === false)
+		{
+			echo 'Curl error:' . curl_error($c);
+		}
+		
+		
 		$result = curl_exec ($c); 
+		if(!$result){
+			echo 'middle: null result';
+		}
 		curl_close ($c);
 		echo $result;
 	}
 	
-
 	if(isset($_GET['function'])){//just an example of how this would work assume functions is getSemesters
-		$arr = array('ucid' => $_POST['ucid']);
+		$arr = array();
+		$f = $_GET['function'];
+		if($f=='insertPost'){
+			$ucid = $_POST['ucid'];
+			$crn = $_POST['crn'];
+			$title = $_POST['title'];
+			$txt = = $_POST['txt'];
+			insertPost($ucid,$crn,$title,$txt);
+		}
+		if(isset($_POST['semesterid'])){
+			$arr = array('ucid' => $_POST['ucid'], 'semesterid' => $_POST['semesterid']);
+		}
+		else{
+			$arr = array('ucid' => $_POST['ucid']);
+		}
 		$functionCall=true;
+		//print_r($arr);
 		getJSON($_GET['function'],$arr,$urlPath);
 	}
 ?>
